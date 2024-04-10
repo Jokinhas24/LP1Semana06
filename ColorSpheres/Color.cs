622822fe7ded585177bacc0e2a9c366534a751f9
João Nogueira <joaonogueira.art@gmail.com>
2024-04-10 11:19:14 +0100

    Created the Color class and it's variables

---
+++ ColorSpheres/Color.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColorSpheres
+{
+    public class Color
+    {
+        public int Red { 0 ; 255};
+        public int Green { 0 ; 255};
+        public int Blue { 0 ; 255};
+        public int Alpha { 0 ; 255};
+    }
+
+}
