3f3c5f8dd3e76867ffcd9c6016554c69dd242e19
João Nogueira <joaonogueira.art@gmail.com>
2024-04-12 13:04:36 +0100

    Created winrate counter and the player constructor

--- PlayerStats/Player.cs
+++ PlayerStats/Player.cs
@@ -8,10 +8,11 @@ namespace PlayerStats
 {
     public class Player
     {
-        private string name { get; set; }
+        private readonly string name;
         private float highScore { get; set; }
         private int playedGames { get; set; }
-        private readonly float WinRate { get; set; }
+        private int wonGames { get; set; }
+        private readonly float WinRate;
 
         public void HighScore(float newScore, float highScore)
         {
@@ -20,10 +21,33 @@ namespace PlayerStats
                 highScore = newScore;
             }
         }
+        public readonly float WinRate()
+        {
+            if (playedGames > 0)
+            {
+                return (float)(playedGames / wonGames);
+            }
+            else
+            {
+                return string ("This Player hasn't played any games");
+            }
+        }
         public void PlayGame(bool win)
         {
             playedGames += 1;
-            
+            if (win == true)
+            {
+                wonGames += 1;
+            }
+
         }
+        Player(string name)
+        {
+            this.name = name;
+            this.highScore = 0;
+            this.playedGames = 0;
+            this.wonGames = 0;
+        }
+    
     }
 }
