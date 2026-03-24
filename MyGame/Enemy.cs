1c01b21b9929716de4f1ca347b320ca13b8bdf39
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-03-24 12:37:14 +0000

    Add methods and an instance

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -1,12 +1,38 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace MyGame
 {
     public class Enemy
     {
+        private string name;
+        private float health;
+        private float shield;
+
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
         
+        public string GetName()
+        {
+            return name;
+        }
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
     }
 }
