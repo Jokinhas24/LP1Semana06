1d679e127dbced818da3d7983a075a9e3cff6977
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-03-24 12:25:48 +0000

    Build mygame and other exercises

---
+++ Chronos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch chrono1 = new Stopwatch();
+            Stopwatch chrono2 = new Stopwatch();
+
+            chrono1.Start();
+            Thread.Sleep(500);
+            chrono2.Start();
+            Thread.Sleep(250);
+
+            chrono1.Stop();
+            chrono2.Stop();
+
+            Console.WriteLine($"Chrono1: {chrono1.ElapsedMilliseconds/1000:f4}");
+            Console.WriteLine($"Chrono2: {chrono2.ElapsedMilliseconds/1000:f4}");
+        }
+    }
+}
---
+++ RandDice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+
+            Random dice = new Random(s);
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += dice.Next(1, 7);
+            }
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
